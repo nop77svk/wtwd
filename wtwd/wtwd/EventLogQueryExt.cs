ae435d44a6dcdcc9af36859bfaa83eb626820b35
pierre@audrey <nop77svk@gmail.com>
2022-07-11 19:26:29 +0200

    ➕ "Kernel-Boot" WEL query

---
+++ wtwd/wtwd/EventLogQueryExt.cs
@@ -0,0 +1,18 @@
+﻿namespace wtwd;
+
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+
+internal static class EventLogQueryExt
+{
+    internal static IEnumerable<EventRecord> AsEnumerable(this EventLogQuery query)
+    {
+        using EventLogReader reader = new EventLogReader(query);
+        while (true)
+        {
+            using EventRecord? row = reader.ReadEvent();
+            if (row == null) break;
+            yield return row;
+        }
+    }
+}
--- wtwd/wtwd/Program.cs
+++ wtwd/wtwd/Program.cs
@@ -1,10 +1,18 @@
-﻿namespace wtwd
+﻿namespace wtwd;
+using System.Diagnostics.Eventing.Reader;
+
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        string oneMonthAgo = DateTime.Now.AddMonths(-1).ToUniversalTime().ToString("O");
+        EventLogQuery? queryKernelBoot = new EventLogQuery("System", PathType.LogName, $"*[System[Provider/@Name = 'Microsoft-Windows-Kernel-Boot' and (EventID = 20 or EventID = 25) and TimeCreated/@SystemTime >= '{oneMonthAgo}']]");
+
+        foreach (var row in queryKernelBoot.AsEnumerable())
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(row);
         }
+
+        Console.WriteLine("Hello, World!");
     }
 }
