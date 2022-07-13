f4578248ded5616ef38245edbffda931ced5b62b
pierre^ <nop77svk@gmail.com>
2022-07-13 10:47:28 +0200

    🧹 unioning of all relevant events simplified

--- wtwd/Program.cs
+++ wtwd/Program.cs
@@ -22,51 +22,29 @@ internal class Program
 
     private static IEnumerable<PcStateChange> ReadEventLogForPcStateChanges(DateTime since)
     {
-        IEnumerable<PcStateChange> result = new List<PcStateChange>();
-
         string sinceAsStr = since.ToUniversalTime().ToString("O");
-        XNamespace eventLogNS = "http://schemas.microsoft.com/win/2004/08/events/event";
 
+        IEnumerable<EventRecord> unionedEvents = new List<EventRecord>();
         EventLogQuery? queryKernelBoot = new EventLogQuery("System", PathType.LogName, $"Event[System[Provider/@Name = 'Microsoft-Windows-Kernel-Boot' and (EventID = 20 or EventID = 25 or EventID = 27) and TimeCreated/@SystemTime >= '{sinceAsStr}']]");
         if (queryKernelBoot != null)
-        {
-            IEnumerable<PcStateChange> queryEvents = queryKernelBoot
-                .AsEnumerable()
-                .Where(x => x.TimeCreated != null)
-                .Select(x => x.AsPcStateChange());
-            result = result.Concat(queryEvents);
-        }
+            unionedEvents = unionedEvents.Concat(queryKernelBoot.AsEnumerable());
 
         EventLogQuery? queryKernelGeneral = new EventLogQuery("System", PathType.LogName, $"Event[System[Provider/@Name = 'Microsoft-Windows-Kernel-General' and (EventID = 12 or EventID = 13) and TimeCreated/@SystemTime >= '{sinceAsStr}']]");
         if (queryKernelGeneral != null)
-        {
-            IEnumerable<PcStateChange> queryEvents = queryKernelGeneral
-                .AsEnumerable()
-                .Where(x => x.TimeCreated != null)
-                .Select(x => x.AsPcStateChange());
-            result = result.Concat(queryEvents);
-        }
+            unionedEvents = unionedEvents.Concat(queryKernelGeneral.AsEnumerable());
 
         EventLogQuery? queryKernelPower = new EventLogQuery("System", PathType.LogName, $"Event[System[Provider/@Name = 'Microsoft-Windows-Kernel-Power' and (EventID = 109 or EventID = 42 or EventID = 107 or Event = 506 or Event = 507) and TimeCreated/@SystemTime >= '{sinceAsStr}']]");
         if (queryKernelPower != null)
-        {
-            IEnumerable<PcStateChange> queryEvents = queryKernelPower
-                .AsEnumerable()
-                .Where(x => x.TimeCreated != null)
-                .Select(x => x.AsPcStateChange());
-            result = result.Concat(queryEvents);
-        }
+            unionedEvents = unionedEvents.Concat(queryKernelPower.AsEnumerable());
 
         EventLogQuery? querySynTpEnhServiceForLockUnlock = new EventLogQuery("Application", PathType.LogName, @"Event[System[Provider/@Name = 'SynTPEnhService' and EventID = 0] and EventData/Data]");
         if (querySynTpEnhServiceForLockUnlock != null)
-        {
-            IEnumerable<PcStateChange> queryEvents = querySynTpEnhServiceForLockUnlock
-                .AsEnumerable()
-                .Where(x => x.TimeCreated != null)
-                .Select(x => x.AsPcStateChange());
-            result = result.Concat(queryEvents);
-        }
+            unionedEvents = unionedEvents.Concat(querySynTpEnhServiceForLockUnlock.AsEnumerable());
 
-        return result;
+        return unionedEvents
+            .Where(evnt => evnt.TimeCreated != null)
+            .Where(evnt => evnt.TimeCreated >= since)
+            .Select(evnt => evnt.AsPcStateChange())
+            .Where(stch => stch.How != PcStateChangeHow.Unknown && stch.What != PcStateChangeWhat.Unknown);
     }
 }
