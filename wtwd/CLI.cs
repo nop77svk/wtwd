6bc5622a416983153f6451345cf916e8b86a3b8a
pierre@audrey <nop77svk@gmail.com>
2022-07-27 22:12:27 +0200

    🔧 CLI.TrimSessionUnder is now cached

--- wtwd/CLI.cs
+++ wtwd/CLI.cs
@@ -9,5 +9,19 @@ internal class CLI
         + "\nUse decimal values with the suffix of \"s\" (seconds), \"m\" (minutes), \"h\" (hours) or a mm:ss time span specification.")]
     public string? TrimSessionsUnderStr { get; set; }
 
-    public TimeSpan? TrimSessionsUnder { get => TimeSpanFromString.Parse(TrimSessionsUnderStr); }
+    private bool _isParsedTrimSessionUnder = false;
+    private TimeSpan? _trimSessionUnder = null;
+    public TimeSpan? TrimSessionsUnder
+    {
+        get
+        {
+            if (!_isParsedTrimSessionUnder)
+            {
+                _trimSessionUnder = TimeSpanFromString.Parse(TrimSessionsUnderStr);
+                _isParsedTrimSessionUnder = true;
+            }
+
+            return _trimSessionUnder;
+        }
+    }
 }
