834e719a4d9aee9f3b7285e3c15d8c486de2c315
pierre@audrey <nop77svk@gmail.com>
2022-07-27 21:11:08 +0200

    + CLI option

--- wtwd/CLI.cs
+++ wtwd/CLI.cs
@@ -1,6 +1,10 @@
-﻿namespace wtwd
+﻿namespace wtwd;
+using CommandLine;
+
+internal class CLI
 {
-    internal class CLI
-    {
-    }
+    [Option("trim-sessions-under", Required = false, HelpText = ""
+        + "\nSessions shorter than the supplied threshold are automatically discarded."
+        + "\nUse integer values with the suffix of \"s\" (seconds), \"m\" (minutes), \"h\" (hours) or a mm:ss time span specification.")]
+    public string? TrimSessionsUnder { get; set; }
 }
