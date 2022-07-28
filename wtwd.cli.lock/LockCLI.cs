d932f627a7be49070850a3a1258e2a101c531c56
pierre^ <nop77svk@gmail.com>
2022-07-28 14:43:16 +0200

    ➕ "lock"-verb CLI

---
+++ wtwd.cli.lock/LockCLI.cs
@@ -0,0 +1,8 @@
+namespace wtwd.cli.lock
+{
+    using CommandLine;
+
+    public class LockCLI
+    {
+    }
+}
--- wtwd/Program.cs
+++ wtwd/Program.cs
@@ -1,14 +1,16 @@
 ﻿namespace wtwd;
 using CommandLine;
 using wtwd.cli.list;
+using wtwd.cli.lock;
 
 internal class Program
 {
     internal static int Main(string[] args)
     {
         Parser.Default
-            .ParseArguments<ListCLI>(args)
-            .WithParsed<ListCLI>(cli => ListProgram.Execute(ListConfig.FromRawCLI(cli)));
+            .ParseArguments<ListCLI, LockCLI>(args)
+            .WithParsed<ListCLI>(cli => ListProgram.Execute(ListConfig.FromRawCLI(cli)))
+            .WithParsed<LockCLI>(cli => LockProgram.Execute(LockConfig.FromRawCLI(cli)));
 
         return 0;
     }
