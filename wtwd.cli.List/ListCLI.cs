a882f7e4a63e8386b8d4480cd595b3ff5ea7c262
pierre@audrey <nop77svk@gmail.com>
2022-07-30 13:02:55 +0200

    🔧 "ignore machine-only sessions" flag now without default of false

--- wtwd.cli.List/ListCLI.cs
+++ wtwd.cli.List/ListCLI.cs
@@ -9,8 +9,6 @@ public class ListCLI
         + "\nUse decimal values with the suffix of \"s\" (seconds), \"m\" (minutes), \"h\" (hours) or a mm:ss time span specification.")]
     public string? TrimSessionsUnder { get; set; }
 
-    [Option("ignore-machine-only-sessions", Required = false, Default = false, HelpText = ""
-        + "\nSessions without starting unlock event may be considered purely technical/maintenance."
-        + "\nWhen switched ON, supposed \"machine-only\" sessions are not displayed.")]
+    [Option("ignore-machine-only-sessions", Required = false, HelpText = "Sessions without starting unlock event may be considered purely technical/maintenance. When supplied, supposed \"machine-only\" sessions are not displayed.")]
     public bool IgnoreMachineOnlySessions { get; set; } = false;
 }
