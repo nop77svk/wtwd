24986197fb996060583ba54b29752ff8b5080a04
pierre@audrey <nop77svk@gmail.com>
2022-07-30 10:41:35 +0200

    🛠 "lock" verb now works

--- wtwd.cli.lock/LockProgram.cs
+++ wtwd.cli.lock/LockProgram.cs
@@ -1,4 +1,8 @@
+#pragma warning disable CA1416
 namespace wtwd.cli.Lock;
+using System.Diagnostics;
+using System.Security;
+using wtwd.Model;
 
 public static class LockProgram
 {
@@ -9,5 +13,14 @@ public static class LockProgram
 
     internal static void Execute(LockConfig cli)
     {
+        try
+        {
+            EventLog.WriteEntry(LockUnlockEventLog.SourceName, LockUnlockEventLog.LockMessage, EventLogEntryType.Information, LockUnlockEventLog.LockEventId, LockUnlockEventLog.LockCategory);
+            Console.WriteLine("Lock event successfully logged");
+        }
+        catch (SecurityException e)
+        {
+            throw new SecurityException(LockUnlockEventLog.NeedElevatedPrivilegesErrorMsg, e);
+        }
     }
 }
