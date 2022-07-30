f80be80afb83e320b33fad2ae8d82bcc07fe385f
pierre@audrey <nop77svk@gmail.com>
2022-07-30 10:32:57 +0200

    ➕ "explicit lock/unlock events" needs its own source creation under elevated privileges

---
+++ wtwd.model/LockUnlockEventLog.cs
@@ -0,0 +1,10 @@
+﻿namespace wtwd.Model;
+
+public static class LockUnlockEventLog
+{
+    public const string LocalMachine = ".";
+    public const string LogName = "Application";
+    public const string SourceName = "WTWD";
+
+    public const string NeedElevatedPrivilegesErrorMsg = "Cannot write custom event logs.\n\nPlease, re-run (with elevated privileges) as\n\n\twtwd init-lock-unlock\n\nto set up new event log source for explicit lock/unlock logging";
+}
--- wtwd/Program.cs
+++ wtwd/Program.cs
@@ -3,16 +3,18 @@ using CommandLine;
 using wtwd.cli.List;
 using wtwd.cli.Lock;
 using wtwd.cli.Unlock;
+using wtwd.cli.InitLockUnlock;
 
 internal class Program
 {
     internal static int Main(string[] args)
     {
         Parser.Default
-            .ParseArguments<ListCLI, LockCLI, UnlockCLI>(args)
+            .ParseArguments<ListCLI, LockCLI, UnlockCLI, InitLockUnlockCLI>(args)
             .WithParsed<ListCLI>(cli => ListProgram.Execute(cli))
             .WithParsed<LockCLI>(cli => LockProgram.Execute(cli))
-            .WithParsed<UnlockCLI>(cli => UnlockProgram.Execute(cli));
+            .WithParsed<UnlockCLI>(cli => UnlockProgram.Execute(cli))
+            .WithParsed<InitLockUnlockCLI>(cli => InitLockUnlockProgram.Execute(cli));
 
         return 0;
     }
