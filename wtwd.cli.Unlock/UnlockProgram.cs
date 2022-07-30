1fd7386c58afc9adbbb574ed8e1f63dd9311bf9d
pierre@audrey <nop77svk@gmail.com>
2022-07-30 10:44:14 +0200

    🔧

--- wtwd.cli.Unlock/UnlockProgram.cs
+++ wtwd.cli.Unlock/UnlockProgram.cs
@@ -1,4 +1,9 @@
+#pragma warning disable CA1416
 namespace wtwd.cli.Unlock;
+
+using System.Diagnostics;
+using System.Security;
+using wtwd.Model;
 
 public static class UnlockProgram
 {
@@ -9,5 +14,14 @@ public static class UnlockProgram
 
     internal static void Execute(UnlockConfig cli)
     {
+        try
+        {
+            EventLog.WriteEntry(LockUnlockEventLog.SourceName, LockUnlockEventLog.UnlockMessage, EventLogEntryType.Information, LockUnlockEventLog.UnlockEventId, LockUnlockEventLog.UnlockCategory);
+            Console.WriteLine("Unlock event successfully logged");
+        }
+        catch (SecurityException e)
+        {
+            throw new SecurityException(LockUnlockEventLog.NeedElevatedPrivilegesErrorMsg, e);
+        }
     }
 }
