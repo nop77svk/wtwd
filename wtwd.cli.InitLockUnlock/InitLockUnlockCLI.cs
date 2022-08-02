de8c4b466b3af55808682a34ed031200d6d3e5b7
pierre^ <nop77svk@gmail.com>
2022-08-02 10:23:51 +0200

    🔧 some notes+warnings added to the CLI help texts

--- wtwd.cli.InitLockUnlock/InitLockUnlockCLI.cs
+++ wtwd.cli.InitLockUnlock/InitLockUnlockCLI.cs
@@ -1,7 +1,7 @@
 ﻿namespace NoP77svk.wtwd.cli.InitLockUnlock;
 using CommandLine;
 
-[Verb("init-lock-unlock", HelpText = "Initialize Windows event log for explicit/custom lock/unlock events source")]
+[Verb("init-lock-unlock", HelpText = "Initialize Windows event log for explicit/custom lock/unlock events source.\nWarning: Requires elevated privileges!")]
 public class InitLockUnlockCLI
 {
     [Option('x', "exe-path", Required = false, HelpText = "Path to wtwd.exe.\nIf not supplied, path to this executable being run is used.")]
--- wtwd.cli.Lock/LockCLI.cs
+++ wtwd.cli.Lock/LockCLI.cs
@@ -1,7 +1,7 @@
 namespace NoP77svk.wtwd.cli.Lock;
 using CommandLine;
 
-[Verb("lock", HelpText = "Post an explicit, custom \"Lock\" event to the Windows event log")]
+[Verb("lock", HelpText = "Post an explicit, custom \"Lock\" event to the Windows event log.\nRequires first-time initialization via init-lock-unlock action.")]
 public class LockCLI
 {
 }
