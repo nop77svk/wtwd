6d4ed2548075751dc86ebfb34f359a84c892ffa6
pierre@audrey <nop77svk@gmail.com>
2022-08-19 16:43:39 +0200

    ➕ simplistic version of SQL's "match_recognize" for IEnumerable<T>

--- wtwd.Utilities/IEnumerableExt.cs
+++ wtwd.Utilities/IEnumerableExt.cs
@@ -2,13 +2,7 @@ namespace NoP77svk.wtwd.Utilities;
 
 public static class IEnumerableExt
 {
-    public static IEnumerable<(T Current, T? Lagged)> Lag<T>(this IEnumerable<T> collection, int lagSize = 1)
-    {
-        return collection.Lag(_ => 0, lagSize);
-    }
-
-    public static IEnumerable<(TElement Current, TElement? Lagged)> Lag<TElement, TPartitionKey>(this IEnumerable<TElement> collection, Func<TElement, TPartitionKey> getPartitionKey, int lagSize = 1)
-        where TPartitionKey : IEquatable<TPartitionKey>
+    public static IEnumerable<(TElement Current, TElement? Lagged)> Lag<TElement>(this IEnumerable<TElement> collection, Func<TElement, TElement, bool> areInTheSamePartition, int lagSize = 1)
     {
         if (lagSize < 0)
             throw new ArgumentOutOfRangeException(nameof(lagSize), lagSize, "Non-negative integer expected");
@@ -19,7 +13,7 @@ public static class IEnumerableExt
         {
             if (previousElement == null)
                 laggedElements.Clear();
-            else if (!getPartitionKey(element).Equals(getPartitionKey(previousElement)))
+            else if (!areInTheSamePartition(element, previousElement))
                 laggedElements.Clear();
 
             TElement? resultLaggedElement;
@@ -32,6 +26,29 @@ public static class IEnumerableExt
 
             laggedElements.Enqueue(element);
             previousElement = element;
+        }
+    }
+
+    public static IEnumerable<(T Current, T? Lagged)> Lag<T>(this IEnumerable<T> collection, int lagSize = 1)
+    {
+        return collection.Lag((_, _) => true, lagSize);
+    }
+
+    public static IEnumerable<(TElement Current, TElement? Lagged)> Lag<TElement, TPartitionKey>(this IEnumerable<TElement> collection, Func<TElement, TPartitionKey> getPartitionKey, int lagSize = 1)
+        where TPartitionKey : IEquatable<TPartitionKey>
+    {
+        return collection.Lag((element, previousElement) => getPartitionKey(element).Equals(getPartitionKey(previousElement)));
+    }
+
+    public static IEnumerable<(int RunId, TElement Element)> RecognizeElementRuns<TElement>(this IEnumerable<TElement> collection, Func<TElement, TElement, bool> areInTheSameRun)
+    {
+        int runId = 0;
+        foreach ((TElement Current, TElement? Lagged) elementTuple in collection.Lag())
+        {
+            bool newRunStartsHere = elementTuple.Lagged == null || !areInTheSameRun(elementTuple.Current, elementTuple.Lagged);
+            if (newRunStartsHere)
+                runId++;
+            yield return (runId, elementTuple.Current);
         }
     }
 }
