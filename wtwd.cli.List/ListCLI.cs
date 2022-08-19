440d4270cf6bc75888115571e846963ca893d24a
pierre@remote <nop77svk@gmail.com>
2022-08-19 12:10:48 +0200

    ➕ CLI for "trim breaks under a specified threshold"

--- wtwd.cli.List/ListCLI.cs
+++ wtwd.cli.List/ListCLI.cs
@@ -9,6 +9,11 @@ public class ListCLI
         + "\nUse decimal values with the suffix of \"s\" (seconds), \"m\" (minutes), \"h\" (hours) or a mm:ss time span specification.")]
     public string? TrimSessionsUnder { get; set; }
 
+    [Option("trim-breaks-under", Required = false, Default = "1:30", HelpText = ""
+        + "\nBreaks shorter than the supplied threshold are automatically discarded and the surrounding sessions are merged."
+        + "\nUse decimal values with the suffix of \"s\" (seconds), \"m\" (minutes), \"h\" (hours) or a mm:ss time span specification.")]
+    public string? TrimBreaksUnder { get; set; }
+
     [Option("ignore-machine-only-sessions", Required = false, HelpText = "Sessions without starting unlock event may be considered purely technical/maintenance. When supplied, supposed \"machine-only\" sessions are not displayed.")]
     public bool IgnoreMachineOnlySessions { get; set; } = false;
 }
--- wtwd.cli.List/ListConfig.cs
+++ wtwd.cli.List/ListConfig.cs
@@ -8,11 +8,14 @@ internal class ListConfig
         return new ListConfig()
         {
             TrimSessionsUnder = TimeSpanExt.Parse(cli.TrimSessionsUnder),
+            TrimBreaksUnder = TimeSpanExt.Parse(cli.TrimBreaksUnder),
             AllowMachineOnlySessions = !cli.IgnoreMachineOnlySessions
         };
     }
 
     internal TimeSpan? TrimSessionsUnder { get; init; }
+
+    internal TimeSpan? TrimBreaksUnder { get; init; }
 
     internal bool AllowMachineOnlySessions { get; init; }
 
