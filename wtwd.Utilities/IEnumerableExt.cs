27e6b844cc00c5f397a7d34a73dfe0b4c28ee922
pierre@remote <nop77svk@gmail.com>
2022-08-19 14:08:24 +0200

    🧹 naming of the IEnumerable.Lag() result items switched to init-caps

--- wtwd.Utilities/IEnumerableExt.cs
+++ wtwd.Utilities/IEnumerableExt.cs
@@ -1,30 +1,33 @@
-namespace wtwd.Utilities;
+namespace NoP77svk.wtwd.Utilities;
 
 public static class IEnumerableExt
 {
-    public static IEnumerable<(T, T?)> Lag<T>(this IEnumerable<T> collection, int lagSize = 1)
+    public static IEnumerable<(T Current, T? Lagged)> Lag<T>(this IEnumerable<T> collection, int lagSize = 1)
     {
-        IEnumerable<(T, T?)> result = collection.Lag((_,_) => true, lagSize);
-        return result;
+        return collection.Lag((_, _) => true, lagSize);
     }
 
-    public static IEnumerable<(T, T?)> Lag<T>(this IEnumerable<T> collection, Func<T, T, bool> areElementsInTheSamePartition, int lagSize = 1)
+    public static IEnumerable<(T Current, T? Lagged)> Lag<T>(this IEnumerable<T> collection, Func<T, T, bool> areElementsInTheSamePartition, int lagSize = 1)
     {
         if (lagSize < 0)
             throw new ArgumentOutOfRangeException(nameof(lagSize), lagSize, "Non-negative integer expected");
 
-        Queue<T> laggedElements = new Queue<T>(lagSize + 1);
+        Queue<T> laggedElements = new Queue<T>(lagSize);
         T? previousElement = default(T);
         foreach (T element in collection)
         {
             if (previousElement == null || !areElementsInTheSamePartition(element, previousElement))
                 laggedElements.Clear();
 
-            laggedElements.Enqueue(element);
-            if (!laggedElements.TryDequeue(out T? laggedResult))
+            T? laggedResult;
+            if (laggedElements.Count == lagSize)
+                laggedResult = laggedElements.Dequeue();
+            else
                 laggedResult = default(T);
+
             yield return (element, laggedResult);
 
+            laggedElements.Enqueue(element);
             previousElement = element;
         }
     }
