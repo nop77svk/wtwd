f1dc25123d8409a63daa179e102da979682698c4
pierre@remote <nop77svk@gmail.com>
2024-07-30 08:47:36 +0200

    🔧 multiple code cleanups and refactorings

--- wtwd.cli.List/ListProgram.cs
+++ wtwd.cli.List/ListProgram.cs
@@ -15,6 +15,9 @@ public static class ListProgram
     private const string SessionDisplayIndent = "    ";
     private const string DayFormat = "yyyy-MM-dd";
     private const string TimeFormat = "HH:mm";
+    private const string SessionSpanMinutesFormat = @"hh\:mm";
+    private const string SessionSpanHoursFormat = @"hh\:mm";
+    private const string SessionSpanDaysFormat = @"d\d\ hh\:mm";
 
     public static void Execute(ListCli cli)
     {
@@ -61,20 +64,6 @@ public static class ListProgram
         DisplayTheSessions(pcSessions, roundingInterval);
     }
 
-    private static string FormatDayOffset(DateTime start, DateTime end)
-    {
-        int offset = (int)Math.Round(end.Date.Subtract(start.Date).TotalDays);
-
-        string result = offset switch
-        {
-            < 0 => offset.ToString(@"d"),
-            > 0 => $"+{offset.ToString(@"d")}",
-            0 => string.Empty
-        };
-
-        return result;
-    }
-
     private static void DisplayTheSessions(IEnumerable<PcSession> sessions, TimeSpan roundingInterval)
     {
         var sessionsGroupedByDay = sessions
@@ -90,37 +79,70 @@ public static class ListProgram
 
             foreach (var session in daySessionsOrdered)
             {
-                string msg = SessionToDisplayString(session, roundingInterval);
+                string msg = session.ToDisplayString(roundingInterval);
                 Console.WriteLine($"{SessionDisplayIndent}{msg}");
             }
         }
+    }
+
+    private static string EventIdsOrExpanded(params int[] ids)
+    {
+        IEnumerable<string> idPredicates = ids.Select(id => $"EventID = {id}");
+
+        string result = string.Join(" or ", idPredicates);
+
+        if (ids.Length > 1)
+        {
+            result = $"({result})";
+        }
+
+        return result;
+    }
+
+    private static string FormatDayOffset(DateTime start, DateTime end)
+    {
+        int offset = (int)Math.Round(end.Date.Subtract(start.Date).TotalDays);
+
+        string result = offset switch
+        {
+            < 0 => offset.ToString(@"d"),
+            > 0 => $"+{offset:d}",
+            0 => string.Empty
+        };
+
+        return result;
     }
 
     private static IEnumerable<EventRecord> GetEventLogsSince(DateTime since)
     {
         string sinceAsStr = since.ToUniversalTime().ToString("O");
 
-        IEnumerable<EventRecord> unionedEvents = new List<EventRecord>();
-        EventLogQuery queryKernelBoot = new EventLogQuery("System", PathType.LogName, $"Event[System[Provider/@Name = 'Microsoft-Windows-Kernel-Boot' and (EventID = 20 or EventID = 25 or EventID = 27) and TimeCreated/@SystemTime >= '{sinceAsStr}']]");
-        unionedEvents = unionedEvents.Concat(queryKernelBoot.AsEnumerable());
+        IEnumerable<EventRecord> result = Enumerable.Empty<EventRecord>();
 
-        EventLogQuery queryKernelGeneral = new EventLogQuery("System", PathType.LogName, $"Event[System[Provider/@Name = 'Microsoft-Windows-Kernel-General' and (EventID = 12 or EventID = 13) and TimeCreated/@SystemTime >= '{sinceAsStr}']]");
-        unionedEvents = unionedEvents.Concat(queryKernelGeneral.AsEnumerable());
+        string queryKernelBootStr = $"Event[System[Provider/@Name = 'Microsoft-Windows-Kernel-Boot' and {EventIdsOrExpanded(20, 25, 27)} and TimeCreated/@SystemTime >= '{sinceAsStr}']]";
+        EventLogQuery queryKernelBoot = new EventLogQuery("System", PathType.LogName, queryKernelBootStr);
+        result = result.Concat(queryKernelBoot.AsEnumerable());
 
-        EventLogQuery queryKernelPower = new EventLogQuery("System", PathType.LogName, $"Event[System[Provider/@Name = 'Microsoft-Windows-Kernel-Power' and (EventID = 109 or EventID = 42 or EventID = 107 or EventID = 506 or EventID = 507) and TimeCreated/@SystemTime >= '{sinceAsStr}']]");
-        unionedEvents = unionedEvents.Concat(queryKernelPower.AsEnumerable());
+        string queryKernelGeneralStr = $"Event[System[Provider/@Name = 'Microsoft-Windows-Kernel-General' and {EventIdsOrExpanded(12, 13)} and TimeCreated/@SystemTime >= '{sinceAsStr}']]";
+        EventLogQuery queryKernelGeneral = new EventLogQuery("System", PathType.LogName, queryKernelGeneralStr);
+        result = result.Concat(queryKernelGeneral.AsEnumerable());
 
-        EventLogQuery? querySynTpEnhServiceForLockUnlock = new EventLogQuery("Application", PathType.LogName, @"Event[System[Provider/@Name = 'SynTPEnhService' and EventID = 0] and EventData/Data]");
-        unionedEvents = unionedEvents
+        string queryKernelPowerStr = $"Event[System[Provider/@Name = 'Microsoft-Windows-Kernel-Power' and {EventIdsOrExpanded(109, 42, 107, 506, 507)} and TimeCreated/@SystemTime >= '{sinceAsStr}']]";
+        EventLogQuery queryKernelPower = new EventLogQuery("System", PathType.LogName, queryKernelPowerStr);
+        result = result.Concat(queryKernelPower.AsEnumerable());
+
+        string querySynTpEnhServiceForLockUnlockStr = $"Event[System[Provider/@Name = 'SynTPEnhService' and {EventIdsOrExpanded(0)}] and EventData/Data]";
+        EventLogQuery querySynTpEnhServiceForLockUnlock = new EventLogQuery("Application", PathType.LogName, querySynTpEnhServiceForLockUnlockStr);
+        result = result
             .Concat(querySynTpEnhServiceForLockUnlock.AsEnumerable()
                 .Where(evnt => evnt.TimeCreated >= since)
             );
 
-        EventLogQuery queryExplicitWtwdLockUnlock = new EventLogQuery(LockUnlockEventLog.LogName, PathType.LogName, @$"Event[System[Provider/@Name = '{LockUnlockEventLog.SourceName}' and Task = {LockUnlockEventLog.LockUnlockCategory} and TimeCreated/@SystemTime >= '{sinceAsStr}']]");
+        string queryExplicitWtwdLockUnlockStr = @$"Event[System[Provider/@Name = '{LockUnlockEventLog.SourceName}' and Task = {LockUnlockEventLog.LockUnlockCategory} and TimeCreated/@SystemTime >= '{sinceAsStr}']]";
+        EventLogQuery queryExplicitWtwdLockUnlock = new EventLogQuery(LockUnlockEventLog.LogName, PathType.LogName, queryExplicitWtwdLockUnlockStr);
 
-        WindowsUser user = WindowsUser.Current();
-
-        unionedEvents = unionedEvents.Concat(queryExplicitWtwdLockUnlock.AsEnumerable()
+        WindowsUser osUser = WindowsUser.Current();
+        var queryExplicitWtwdLockUnlockTimeFiltered = queryExplicitWtwdLockUnlock.AsEnumerable()
             .Where(evnt => evnt.TimeCreated >= since)
             .Select(evnt => new ValueTuple<EventRecord, ValueTuple<string?, string?, string?>>(
                 evnt,
@@ -142,17 +164,17 @@ public static class ListProgram
                         func: (accumulator, value) => (accumulator.Item1 ?? value.Item1, accumulator.Item2 ?? value.Item2, accumulator.Item3 ?? value.Item3)
                     )
             ))
-            .Where(evntPlus => evntPlus.Item2.Item1 == user.Domain && evntPlus.Item2.Item2 == user.Name
-                || evntPlus.Item2.Item3 == user.SID
+            .Where(evntPlus => evntPlus.Item2.Item1 == osUser.Domain && evntPlus.Item2.Item2 == osUser.Name
+                || evntPlus.Item2.Item3 == osUser.SID
             )
-            .Select(evntPlus => evntPlus.Item1)
-        );
+            .Select(evntPlus => evntPlus.Item1);
 
-        return unionedEvents
-            .Where(evnt => evnt.TimeCreated >= since);
+        result = result.Concat(queryExplicitWtwdLockUnlockTimeFiltered);
+
+        return result.Where(evnt => evnt.TimeCreated >= since);
     }
 
-    private static string SessionToDisplayString(PcSession session, TimeSpan roundingInterval)
+    private static string ToDisplayString(this PcSession session, TimeSpan roundingInterval)
     {
         StringBuilder msg = new StringBuilder();
 
@@ -184,8 +206,11 @@ public static class ListProgram
         {
             msg.Append(" = ");
 
-            string? shortSessionSpanDisp = session.FullSessionSpan?.Add(TimeSpan.FromMinutes(1)).ToVariableString(minutesFormat: @"hh\:mm", hoursFormat: @"hh\:mm", daysFormat: @"d\d\ hh\:mm");
-            string? longSessionSpanDisp = session.FullSessionSpan?.Add(TimeSpan.FromMinutes(1)).ToVariableString(minutesFormat: @"hh\:mm", hoursFormat: @"hh\:mm", daysFormat: @"d\d\ hh\:mm");
+            string? shortSessionSpanDisp = session.FullSessionSpan?.Add(TimeSpan.FromMinutes(1))
+                .ToVariableString(minutesFormat: SessionSpanMinutesFormat, hoursFormat: SessionSpanHoursFormat, daysFormat: SessionSpanDaysFormat);
+
+            string? longSessionSpanDisp = session.FullSessionSpan?.Add(TimeSpan.FromMinutes(1))
+                .ToVariableString(minutesFormat: SessionSpanMinutesFormat, hoursFormat: SessionSpanHoursFormat, daysFormat: SessionSpanDaysFormat);
 
             msg.Append(shortSessionSpanDisp ?? "?");
             if (shortSessionSpanDisp != longSessionSpanDisp)
