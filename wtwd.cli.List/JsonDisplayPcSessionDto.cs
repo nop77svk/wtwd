c8a70cb257be3e6d7ebf80816d3e48167b4991c2
pierre@audrey <nop77svk@gmail.com>
2024-09-16 13:18:16 +0200

    🔧 nullability fix

--- wtwd.cli.List/JsonDisplayPcSessionDto.cs
+++ wtwd.cli.List/JsonDisplayPcSessionDto.cs
@@ -6,7 +6,7 @@ using System;
 internal record JsonDisplayPcSessionDto(DateTime Start, DateTime End)
 {
     public DateTime? FirstStart { get; init; }
-    public IEnumerable<string>? StartEventsOrdered { get; init; }
+    public IEnumerable<string?>? StartEventsOrdered { get; init; }
     public DateTime? LastEnd { get; init; }
-    public IEnumerable<string>? EndEventsOrdered { get; init; }
+    public IEnumerable<string?>? EndEventsOrdered { get; init; }
 }
